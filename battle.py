af8e5841e1a8e2b97ef57fc97e85e4b4f43961c2
aldairlfp <aldairalofnsoperez@gmail.com>
2022-10-05 17:49:09 +0000

    First commit

---
+++ battle.py
@@ -0,0 +1,36 @@
+class Battle:
+    """A game is similar to a problem, but it has a terminal test instead of 
+    a goal test, and a utility for each terminal state. To create a game, 
+    subclass this class and implement `actions`, `result`, `is_terminal`, 
+    and `utility`. You will also need to set the .initial attribute to the 
+    initial state; this can be done in the constructor."""
+
+    def actions(self, state):
+        """Return a collection of the allowable moves from this state."""
+        raise NotImplementedError
+
+    def result(self, state, move):
+        """Return the state that results from making a move from a state."""
+        raise NotImplementedError
+
+    def is_terminal(self, state):
+        """Return True if this is a final state for the game."""
+        return not self.actions(state)
+    
+    def utility(self, state, player):
+        """Return the value of this final state to player."""
+        raise NotImplementedError
+        
+
+def play_game(game, strategies: dict, verbose=False):
+    """Play a turn-taking game. `strategies` is a {player_name: function} dict,
+    where function(state, game) is used to get the player's move."""
+    state = game.initial
+    while not game.is_terminal(state):
+        player = state.to_move
+        move = strategies[player](game, state)
+        state = game.result(state, move)
+        if verbose: 
+            print('Player', player, 'move:', move)
+            print(state)
+    return state
