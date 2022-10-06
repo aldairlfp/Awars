3dd994d34fb7a2bfbdde3815251e85f9fea780e6
aldairlfp <aldairalofnsoperez@gmail.com>
2022-10-06 22:19:16 +0000

    Change all occurrences of game for battle

--- battle.py
+++ battle.py
@@ -1,6 +1,6 @@
 class Battle:
-    """A game is similar to a problem, but it has a terminal test instead of 
-    a goal test, and a utility for each terminal state. To create a game, 
+    """A battle is similar to a problem, but it has a terminal test instead of 
+    a goal test, and a utility for each terminal state. To create a battle, 
     subclass this class and implement `actions`, `result`, `is_terminal`, 
     and `utility`. You will also need to set the .initial attribute to the 
     initial state; this can be done in the constructor."""
@@ -14,7 +14,7 @@ class Battle:
         raise NotImplementedError
 
     def is_terminal(self, state):
-        """Return True if this is a final state for the game."""
+        """Return True if this is a final state for the battle."""
         return not self.actions(state)
     
     def utility(self, state, player):
@@ -22,14 +22,14 @@ class Battle:
         raise NotImplementedError
         
 
-def play_game(game, strategies: dict, verbose=False):
-    """Play a turn-taking game. `strategies` is a {player_name: function} dict,
-    where function(state, game) is used to get the player's move."""
-    state = game.initial
-    while not game.is_terminal(state):
+def simulate_battle(battle, strategies: dict, verbose=False):
+    """Play a turn-taking battle. `strategies` is a {player_name: function} dict,
+    where function(state, battle) is used to get the player's move."""
+    state = battle.initial
+    while not battle.is_terminal(state):
         player = state.to_move
-        move = strategies[player](game, state)
-        state = game.result(state, move)
+        move = strategies[player](battle, state)
+        state = battle.result(state, move)
         if verbose: 
             print('Player', player, 'move:', move)
             print(state)
