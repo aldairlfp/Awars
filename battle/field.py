5bcffb8490c1ab6b3440b4dc5a55b8f2318bb29e
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-11-26 02:39:05 -0500

    Format the file field.py

--- battle/field.py
+++ battle/field.py
@@ -1,17 +1,19 @@
 from collections import defaultdict
 
-class Board(defaultdict):
+
+class Field(defaultdict):
     """A board has the player to move, a cached utility value, 
     and a dict of {(x, y): player} entries, where player is 'X' or 'O'."""
     empty = '.'
     off = '#'
-    
-    def __init__(self, width=8, height=8, to_move=None, **kwds):
-        self.__dict__.update(width=width, height=height, to_move=to_move, **kwds)
-        
-    def new(self, changes: dict, **kwds) -> 'Board':
-        "Given a dict of {(x, y): contents} changes, return a new Board with the changes."
-        board = Board(width=self.width, height=self.height, **kwds)
+
+    def __init__(self, width: int = 8, height: int = 8, **kwargs):
+        super().__init__()
+        self.__dict__.update(width=width, height=height, **kwargs)
+
+    def new(self, changes: dict, **kwds) -> 'Field':
+        """Given a dict of {(x, y): contents} changes, return a new Board with the changes."""
+        board = Field(width=self.width, height=self.height, **kwds)
         board.update(self)
         board.update(changes)
         return board
@@ -22,10 +24,11 @@ class Board(defaultdict):
             return self.empty
         else:
             return self.off
-            
-    def __hash__(self): 
+
+    def __hash__(self):
         return hash(tuple(sorted(self.items()))) + hash(self.to_move)
-    
+
     def __repr__(self):
         def row(y): return ' '.join(self[x, y] for x in range(self.width))
-        return '\n'.join(map(row, range(self.height))) +  '\n'
+
+        return '\n'.join(map(row, range(self.height))) + '\n'
