d8e821eed16bfae75e07f19d943b7cfce431b62e
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-12-28 02:49:16 -0500

    Leave field only be the field with obstacles

--- battle/field.py
+++ battle/field.py
@@ -1,23 +1,11 @@
+import unit
+
+
 class Field:
     def __init__(self, width=10, height=10):
         self.field = {}
-        self.soldiers = {}
-        self.soldiers_count = 0
         self.width = width
         self.height = height
-
-    def add_soldier(self, soldier):
-        self.soldiers_count += 1
-        self.soldiers[soldier.get_posx(), soldier.get_posy()] = soldier
-        self.field.update(self.soldiers)
-
-    def move_soldier(self, soldier, x, y):
-        if (x, y) in self.field:
-            return False
-        else:
-            self.field.pop((soldier.get_posx(), soldier.get_posy()))
-            self.field.update({(x, y): soldier})
-            return True
 
     def __str__(self):
         return self.__repr__()
@@ -28,7 +16,10 @@ class Field:
             for j in range(self.height):
                 str += '  '
                 if (i, j) in self.field:
-                    str += 'S'
+                    if self.field[i, j] in self.soldiers.values():
+                        str += 'S'
+                    else:
+                        str += 'W'
                 else:
                     str += '.'
             str += '\n'
