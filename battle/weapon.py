0aef7bff6fd920ac69e7e23e410e1fdc825f7dca
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-12-28 01:49:27 -0500

    Reformat weapon.py

--- battle/weapon.py
+++ battle/weapon.py
@@ -1,8 +1,9 @@
 class Weapon:
     """Weapon to use for the units in the simulation"""
-    def __init__(self, damage:int, range:int) -> None:
-        self._damage = damage   # The weapon's damage
-        self._range = range     # The weapon's range
+
+    def __init__(self, damage: int, range: int) -> None:
+        self._damage = damage  # The weapon's damage
+        self._range = range  # The weapon's range
 
     def damage(self):
         return self._damage
@@ -10,11 +11,13 @@ class Weapon:
     def range(self):
         return self._range
 
+
 class FireWeapon(Weapon):
     """Fire weapon to use for the units in the simulation"""
-    def __init__(self, damage:int, range:int, ammo:int) -> None:
+
+    def __init__(self, damage: int, range: int, ammo: int) -> None:
         super().__init__(damage, range)
-        self._ammo = ammo       # The weapon's ammo
+        self._ammo = ammo  # The weapon's ammo
 
     def ammo(self):
         return self._ammo
