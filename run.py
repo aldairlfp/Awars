28849405393ed0d904051ccb12d03b5084060f28
Mauro-Bolado <maurovizoso@gmail.com>
2023-01-01 20:23:28 -0500

    modified:   run.py

--- run.py
+++ run.py
@@ -14,18 +14,12 @@ if __name__ == "__main__":
     for i in range(5):
         gen = Normal_generator(i, "random", "blue")
         unit = gen.generate_unit()
-        unit.set_strategies(strategies)
+        unit.strategies(strategies)
         units.append(unit)
         
     for i in range(5):
         gen = Normal_generator(i, "random", "red")
         unit = gen.generate_unit()
-        unit.set_strategies(strategies)
+        unit.strategies(strategies)
         units.append(unit)
         
-    sim.se
-        
-    terrain = board.Board(50, 50, Basic_map(50, 50), Random_allocator(strategies, None))
-    terrain.create_map()
-    terrain.allocate_units()
-    sim.execute()
