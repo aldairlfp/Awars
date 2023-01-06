16d69015b8f74cb65457f8403fc58ef76fc97bc3
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2023-01-06 15:53:34 -0500

    Working good the lexer

--- language/lexer.py
+++ language/lexer.py
@@ -54,84 +54,78 @@ tokens = [
              'FLOAT_LITERAL',
          ] + list(reserved.values())
 
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_ASSIGN = r'='
-t_SEMI = r';'
-t_COMMA = r','
-t_LBRACE = r'{'
-t_RBRACE = r'}'
-t_LBRACKET = r'\['
-t_RBRACKET = r'\]'
-t_LT = r'<'
-t_GT = r'>'
-t_LTE = r'<='
-t_GTE = r'>='
-t_EQ = r'=='
-t_NEQ = r'!='
-t_AND = r'&&'
-t_OR = r'\|\|'
-t_NOT = r'!'
-t_STRING_LITERAL = r'\".*\"'
-t_CHAR_LITERAL = r'\'.\''
-t_FLOAT_LITERAL = r'\d+\.\d+'
 
+def aw_lexer():
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_TIMES = r'\*'
+    t_DIVIDE = r'/'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_ASSIGN = r'='
+    t_SEMI = r';'
+    t_COMMA = r','
+    t_LBRACE = r'{'
+    t_RBRACE = r'}'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LT = r'<'
+    t_GT = r'>'
+    t_LTE = r'<='
+    t_GTE = r'>='
+    t_EQ = r'=='
+    t_NEQ = r'!='
+    t_AND = r'&&'
+    t_OR = r'\|\|'
+    t_NOT = r'!'
+    t_STRING_LITERAL = r'\".*\"'
+    t_CHAR_LITERAL = r'\'.\''
+    t_FLOAT_LITERAL = r'\d+\.\d+'
 
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')
-    return t
+    def t_ID(t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        t.type = reserved.get(t.value, 'ID')
+        return t
 
+    def t_FUNCTION(t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*\('
+        t.type = reserved.get(t.value, 'FUNCTION')
+        return t
 
-def t_FUNCTION(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*\('
-    t.type = reserved.get(t.value, 'FUNCTION')
-    return t
+    def t_NUMBER(t):
+        r'\d+'
+        t.value = float(t.value)
+        return t
 
+    def t_COMMENT(t):
+        r'//.*'
+        pass
 
-def t_NUMBER(t):
-    r'\d+'
-    t.value = float(t.value)
-    return t
+    def t_newline(t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
 
+    def find_column(input, token):
+        """
+        Compute the column.
+        @param input: the input text string
+        @param token: a token instance
+        @return: the column number
+        """
+        line_start = input.rfind('\n', 0, token.lexpos) + 1
+        return (token.lexpos - line_start) + 1
 
-def t_COMMENT(t):
-    r'//.*'
-    pass
+    t_ignore = ' \t'
 
+    def t_error(t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+    def t_eof(t):
+        return None
 
+    return lex.lex()
 
-def find_column(input, token):
-    """
-    Compute the column.
-    @param input: the input text string
-    @param token: a token instance
-    @return: the column number
-    """
-    line_start = input.rfind('\n', 0, token.lexpos) + 1
-    return (token.lexpos - line_start) + 1
-
-
-t_ignore = ' \t'
-
-
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
-
-def t_eof(t):
-    return None
-
-
-lexer = lex.lex()
 
 data = '''
 s=1;
@@ -146,6 +140,7 @@ soldier a = 5;
 soldier b = 6;
 '''
 
+lexer = aw_lexer()
 lexer.input(data1)
 
 while True:
