d2141ff1e3021febde499e43e1374b9b02f7e4f6
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2023-01-10 00:50:43 -0500

    Assignement working good, now working on functions

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,15 +1,21 @@
-import lexer
 import ply.yacc as yacc
 from lexer import tokens
+from compiler.ast import (ProgramNode, PrintNode, StatementNode, VarDeclarationNode, FunctionDeclarationNode, PlusNode,
+    MinusNode, StarNode, DivNode, ConstantNumNode)
+
 
 def aw_parser():
     def p_program(p):
-        'program : statement_list'
+        'program : function'
         p[0] = p[1]
 
+    def p_epsilon(p):
+        'epsilon :'
+        pass
+
     def p_statement_list(p):
-        '''statement_list : statement semi statement_list
-                          | statement'''
+        '''statement_list : statement SEMI statement_list
+                           | statement'''
         if len(p) == 2:
             p[0] = [p[1]]
         else:
@@ -21,17 +27,25 @@ def aw_parser():
                      | expression'''
         p[0] = p[1]
 
+    def p_statement_epsilon(p):
+        'statement : epsilon'
+        p[0] = []
+
     def p_semi(p):
         'semi : SEMI'
         pass
 
+    def p_newline(p):
+        'newline : NEWLINE'
+        pass
+
     def p_assignment(p):
-        'assignment : ID ASSIGN expression'
-        p[0] = ('=', p[1], p[3])
+        'assignment : NUMBERTYPE ID ASSIGN expression'
+        p[0] = VarDeclarationNode(p[2], p[4])
 
     def p_function(p):
         'function : FUNCTION ID LPAREN params RPAREN LBRACE statement_list RBRACE'
-        p[0] = ('function', p[2], p[4], p[7])
+        p[0] = FunctionDeclarationNode(p[2], p[4], p[7])
 
     def p_params(p):
         '''params : param COMMA params
@@ -42,8 +56,8 @@ def aw_parser():
             p[0] = [p[1]] + p[3]
 
     def p_param(p):
-        'param : TYPE ID'
-        p[0] = (p[1], p[2])
+        'param : NUMBER ID'
+        p[0] = p[1]
 
     def p_expression(p):
         '''expression : expression PLUS term
@@ -53,27 +67,27 @@ def aw_parser():
             p[0] = p[1]
         else:
             if p[2] == '+':
-                p[0] = ('+', p[1], p[3])
+                p[0] = PlusNode(p[1], p[3])
             else:
-                p[0] = ('-', p[1], p[3])
+                p[0] = MinusNode(p[1], p[3])
 
     def p_term(p):
-        '''term : term TIMES factor
+        '''term : term STAR factor
                 | term DIVIDE factor
                 | factor'''
         if len(p) == 2:
             p[0] = p[1]
         else:
             if p[2] == '*':
-                p[0] = ('*', p[1], p[3])
+                p[0] = StarNode(p[1], p[3])
             else:
-                p[0] = ('/', p[1], p[3])
+                p[0] = DivNode(p[1], p[3])
 
     def p_factor(p):
         '''factor : NUMBER
                   | LPAREN expression RPAREN'''
         if len(p) == 2:
-            p[0] = p[1]
+            p[0] = ConstantNumNode(p[1])
         else:
             p[0] = p[2]
 
