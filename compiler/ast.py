ff875ca276078f559ebf6d7ad0e651f2e7098e4d
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2023-01-09 19:19:54 -0500

    Reformat ast.py

--- compiler/ast.py
+++ compiler/ast.py
@@ -1,20 +1,25 @@
 class Node:
     pass
 
+
 class ProgramNode(Node):
     def __init__(self, statements):
         self.statements = statements
 
+
 class StatementNode(Node):
     pass
 
+
 class ExpressionNode(Node):
     pass
+
 
 class VarDeclarationNode(StatementNode):
     def __init__(self, id, expression):
         self.id = id
         self.expression = expression
+
 
 class FunctionDeclarationNode(StatementNode):
     def __init__(self, id, params, body):
@@ -22,38 +27,48 @@ class FunctionDeclarationNode(StatementNode):
         self.params = params
         self.body = body
 
+
 class PrintNode(StatementNode):
     def __init__(self, expression):
         self.expression = expression
 
+
 class AtomicNode(ExpressionNode):
     def __init__(self, lex):
         self.lex = lex
+
 
 class BinaryNode(ExpressionNode):
     def __init__(self, left, right):
         self.left = left
         self.right = right
 
+
 class ConstantNumNode(AtomicNode):
     pass
 
+
 class VariableNode(AtomicNode):
     pass
+
 
 class CallNode(AtomicNode):
     def __init__(self, id, args):
         AtomicNode.__init__(self, id)
         self.args = args
 
+
 class PlusNode(BinaryNode):
     pass
+
 
 class MinusNode(BinaryNode):
     pass
 
+
 class StarNode(BinaryNode):
     pass
+
 
 class DivNode(BinaryNode):
     pass
