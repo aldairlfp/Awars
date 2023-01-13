a8072419b26d1fddf00deec690279ae3805488b8
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2023-01-13 02:53:26 -0500

    Added variable info and function info to the context

---
+++ compiler/context.py
@@ -0,0 +1,9 @@
+class VariableInfo:
+    def __init__(self, name):
+        self.name = name
+
+
+class FunctionInfo:
+    def __init__(self, name, params):
+        self.name = name
+        self.params = params
