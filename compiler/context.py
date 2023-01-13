126bcfbb36422a38f859ae1ed132bc10009a28b5
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2023-01-13 02:57:33 -0500

    Added scope to context

--- compiler/context.py
+++ compiler/context.py
@@ -1,3 +1,5 @@
+import itertools as itl
+
 class VariableInfo:
     def __init__(self, name):
         self.name = name
@@ -7,3 +9,49 @@ class FunctionInfo:
     def __init__(self, name, params):
         self.name = name
         self.params = params
+
+class Scope:
+    def __init__(self, parent=None):
+        self.local_vars = []
+        self.local_funcs = []
+        self.parent = parent
+        self.children = []
+        self.var_index_at_parent = 0 if parent is None else len(parent.local_vars)
+        self.func_index_at_parent = 0 if parent is None else len(parent.local_funcs)
+
+    def create_child_scope(self):
+        child_scope = Scope(self)
+        self.children.append(child_scope)
+        return child_scope
+
+    def define_variable(self, vname):
+        self.local_vars.append(VariableInfo(vname))
+        return
+
+    def define_function(self, fname, params):
+        self.local_funcs.append(FunctionInfo(fname, params))
+        return
+
+    def is_var_defined(self, vname):
+        for var in self.local_vars:
+            if var.name == vname:
+                return True
+        return False
+
+    def is_func_defined(self, fname, n):
+        for func in self.local_funcs:
+            if func.name == fname and len(func.params) == n:
+                return True
+        return False
+
+    def is_local_var(self, vname):
+        return self.get_local_variable_info(vname) is not None
+
+    def is_local_func(self, fname, n):
+        return self.get_local_function_info(fname, n) is not None
+
+    def get_local_variable_info(self, vname):
+        return next((var for var in self.local_vars if var.name == vname), None)
+
+    def get_local_function_info(self, fname, n):
+        return next((func for func in self.local_funcs if func.name == fname and len(func.params) == n), None)
